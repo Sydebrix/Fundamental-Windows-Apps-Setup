99ef3384910448e4d6d094e068f3ddfe3ac64f90
Sydebrix <robeat94@googlemail.com>
2026-08-28 11:18:41 +0200

    Add profile definitions

---
+++ packages/dev.ps1
@@ -0,0 +1,23 @@
+[CmdletBinding()]
+param(
+    [ValidateSet("Auto", "Online", "Offline")]
+    [string]$Mode = "Auto",
+
+    [ValidateSet("x64", "x86", "arm64")]
+    [string]$Architecture = "x64",
+
+    [switch]$InteractiveOfflineFallback
+)
+
+$invokeArgs = @{
+    Profile = "Dev"
+    Mode = $Mode
+    Architecture = $Architecture
+}
+
+if ($InteractiveOfflineFallback) {
+    $invokeArgs.InteractiveOfflineFallback = $true
+}
+
+& (Join-Path $PSScriptRoot "..\install.ps1") @invokeArgs
+exit $LASTEXITCODE
